95588d4938e57e3923be37ac9d80add9bb42fc84
osmanmo <osmanomohamed@gmail.com>
2021-02-11 14:22:15 -0600

    Add files via upload

---
+++ rebar_info.py
@@ -0,0 +1,28 @@
+filename='C:/User121/infile.csv'
+from detail5 import*
+from matplotlib import pyplot as plt
+
+
+height=12
+rebar=['2#5 Footing',
+       '#4 MK401 Slab DWLS@48"',
+       '2#5 Turndown',
+     
+      
+      ]
+
+footing_width=0
+color='magenta'
+make_detail(filename,footing_width,rebar,color,height)
+
+
+
+
+
+
+
+
+
+plt.show()
+
+
